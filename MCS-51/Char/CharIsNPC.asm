2238bcb2d5d24b3e437bee7302fd03572161c5ca
ElectronWranglerII <128319092+ElectronWranglerII@users.noreply.github.com>
2023-03-19 22:46:30 -0700

    Create CharIsNPC.asm

---
+++ MCS-51/Char/CharIsNPC.asm
@@ -0,0 +1,37 @@
+;* Yggdrasil (TM) Core Operating System (MCS-51): Character Library
+;* Copyright (C) DeRemee Systems, IXE Electronics LLC
+;* Portions copyright IXE Electronics LLC, Republic Robotics,
+;* FemtoLaunch, FemtoSat, FemtoTrack, Weland
+;* This work is made available under the Creative Commons
+;* Attribution-NonCommercial-ShareAlike 4.0 International License.
+;* To view a copy of this license, visit
+;* http://creativecommons.org/licenses/by-nc-sa/4.0/.
+
+;RETURNS TRUE IF VALUE IS A NON-PRINTING CHARACTER
+;ON ENTRY:
+;	A = VALUE
+;ON RETURN:
+;	A = VALUE ON ENTRY
+;	C = 0 IF VALUE IS NOT A NON-PRINTING CHARACTER
+;	C = 1 IF VALUE IS A NON-PRINTING CHARACTER
+CHARISNPC:
+	;SAVE REGISTERS
+	XCH		A, R0
+	PUSH	ACC
+	;CHECK FOR LESS THAN 0x20
+	MOV		A, R0
+	CLR		C
+	SUBB	A, #0x20
+	JC		CHARISNPCA
+	;CHECK FOR 0x7F
+	MOV		A, #0x7F
+	XRL		A, R0
+	JZ		CHARISNPCA
+	POP		ACC
+	XCH		A, R0
+	RET
+CHARISNPCA:
+	POP		ACC
+	XCH		A, R0
+	SETB	C
+	RET
