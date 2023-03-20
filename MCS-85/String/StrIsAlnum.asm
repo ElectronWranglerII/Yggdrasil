e213a538d9e06ad208316c5eb7a7c16d4027a951
ElectronWranglerII <128319092+ElectronWranglerII@users.noreply.github.com>
2023-03-19 19:23:10 -0700

    Create StrIsAlnum.asm

---
+++ MCS-85/String/StrIsAlnum.asm
@@ -0,0 +1,66 @@
+;* Yggdrasil (TM) Core Operating System (MCS-80/85): String Library
+;* Copyright (C) DeRemee Systems, IXE Electronics LLC
+;* Portions copyright IXE Electronics LLC, Republic Robotics,
+;* FemtoLaunch, FemtoSat, FemtoTrack, Weland
+;* This work is made available under the Creative Commons
+;* Attribution-NonCommercial-ShareAlike 4.0 International License.
+;* To view a copy of this license, visit
+;* http://creativecommons.org/licenses/by-nc-sa/4.0/.
+
+;RETURNS TRUE IF THE STRING CONTAINS ONLY LETTER AND NUMBER CHARACTERS
+;ON ENTRY:
+;	B		= TERMINATOR
+;	HL		= SOURCE STRING ADDRESS
+;	[HL]	= SOURCE STRING
+;ON RETURN:
+;	A		= 0x00
+;	B		= VALUE ON ENTRY
+;	HL		= VALUE ON ENTRY
+;	[HL]	= VALUE ON ENTRY
+;	CF		= 0 IF NON-ALPHANUMERIC CHARACTERS FOUND
+;	CF		= 1 IF NO NON-ALPHANUMERIC CHARACTERS FOUND
+STRISALNUM:
+	;SAVE REGISTERS
+	PUSH	PSW
+	PUSH	H
+STRISALNUMA:
+	;LOAD CHARACTER FROM STRING
+	MOV		A, M
+	;CHARACTER == TERMINATOR?
+	CMP		B
+	JZ		STRISALNUMD
+	;CHARACTER < "0"?
+	CPI		0x30
+	JC		STRISALNUMC
+	;CHARACTER > "z"?
+	CPI		0x7B
+	JNC		STRISALNUMC
+	;CHARACTER > "9"?
+	CPI		0x3A
+	JC		STRISALNUMB
+	;CHARACTER < "A"?
+	CPI		0x41
+	JC		STRISALNUMC
+	;CHARACTER > "Z"?
+	CPI		0x5B
+	JC		STRISALNUMB
+	;CHARACTER < "a"?
+	CPI		0x61
+	JC		STRISALNUMC
+STRISALNUMB:
+	;POINT TO NEXT CHARACTER
+	INX		H
+	JMP		STRISALNUMA
+STRISALNUMC:
+	;RESTORE REGISTERS & RETURN
+	POP		H
+	POP		PSW
+	XRA		A
+	RET
+STRISALNUMD:
+	;RESTORE REGISTERS & RETURN
+	POP		H
+	POP		PSW
+	XRA		A
+	STC
+	RET
