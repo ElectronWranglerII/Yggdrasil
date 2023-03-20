76ec6b9b1fe23bbbeffff509d4f3c4873439a643
ElectronWranglerII <128319092+ElectronWranglerII@users.noreply.github.com>
2023-03-19 23:11:31 -0700

    Create CharFromInt.asm

---
+++ x86/Char/CharFromInt.asm
@@ -0,0 +1,27 @@
+;* Yggdrasil (TM) Core Operating System (x86): Character Library
+;* Copyright (C) DeRemee Systems, IXE Electronics LLC
+;* Portions copyright IXE Electronics LLC, Republic Robotics,
+;* FemtoLaunch, FemtoSat, FemtoTrack, Weland
+;* This work is made available under the Creative Commons
+;* Attribution-NonCommercial-ShareAlike 4.0 International License.
+;* To view a copy of this license, visit
+;* http://creativecommons.org/licenses/by-nc-sa/4.0/.
+
+;CONVERTS A SINGLE DIGIT INTEGER TO A CHARACTER
+;ON ENTRY:
+;	AL = SINGLE DIGIT INTEGER
+;ON RETURN:
+;	AL = VALUE ON ENTRY
+;	C = 0 IF VALUE IS AN INTEGER
+;	C = 1 IF VALUE IS NOT AN INTEGER
+CHARFROMINT:
+	;VALUE > 0x09?
+	CMP		AL, 0x0A
+	JNC		CHARFROMINTA
+	;CONVERT TO CHARACTER
+	ADD		AL, 0x30
+	RET
+CHARFROMINTA:
+	XOR		AL, AL
+	STC
+	RET
