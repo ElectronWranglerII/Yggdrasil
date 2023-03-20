0ef522d7dd16ac76be2f50d6d59feb2a2522be3c
ElectronWranglerII <128319092+ElectronWranglerII@users.noreply.github.com>
2023-03-19 19:27:29 -0700

    Create StrIsNum.asm

---
+++ MCS-85/String/StrIsNum.asm
@@ -0,0 +1,54 @@
+;* Yggdrasil (TM) Core Operating System (MCS-80/85): String Library
+;* Copyright (C) DeRemee Systems, IXE Electronics LLC
+;* Portions copyright IXE Electronics LLC, Republic Robotics,
+;* FemtoLaunch, FemtoSat, FemtoTrack, Weland
+;* This work is made available under the Creative Commons
+;* Attribution-NonCommercial-ShareAlike 4.0 International License.
+;* To view a copy of this license, visit
+;* http://creativecommons.org/licenses/by-nc-sa/4.0/.
+
+;RETURNS TRUE IF THE STRING CONTAINS ONLY NUMERIC CHARACTERS
+;ON ENTRY:
+;	B		= TERMINATOR
+;	HL		= SOURCE STRING ADDRESS
+;	[HL]	= SOURCE STRING
+;ON RETURN:
+;	A		= 0x00
+;	B		= VALUE ON ENTRY
+;	HL		= VALUE ON ENTRY
+;	[HL]	= VALUE ON ENTRY
+;	CF		= 0 IF NON-NUMERIC CHARACTERS FOUND
+;	CF		= 1 IF NO NON-NUMERIC CHARACTERS FOUND
+STRISNUM:
+	;SAVE REGISTERS
+	PUSH	PSW
+	PUSH	H
+STRISNUMA:
+	;LOAD CHARACTER FROM STRING
+	MOV		A, M
+	;CHARACTER == TERMINATOR?
+	CMP		B
+	JZ		STRISNUMD
+	;CHARACTER < "0"?
+	CPI		0x30
+	JC		STRISNUMC
+	;CHARACTER > "9"?
+	CPI		0x3A
+	JNC		STRISNUMC
+STRISNUMB:
+	;POINT TO NEXT CHARACTER
+	INX		H
+	JMP		STRISNUMA
+STRISNUMC:
+	;RESTORE REGISTERS & RETURN
+	POP		H
+	POP		PSW
+	XRA		A
+	RET
+STRISNUMD:
+	;R4ESTORE REGISTERS & RETURN
+	POP		H
+	POP		PSW
+	XRA		A
+	STC
+	RET
